1fde6c18ca743fe7c230f854f158860cf7ff3e46
Elizeu Vailant <83087208+movingGreen@users.noreply.github.com>
2022-07-28 11:43:32 -0400

    Atividades targetsistemas.

---
+++ targetsistemas/Atividade2.js
@@ -0,0 +1,13 @@
+let entrada = 215;
+
+function isInFibonnaci(numero) {
+    return (numero in Fibonnaci(numero)) ? true : false; 
+}
+
+function Fibonnaci(valorMaximo) {
+    
+
+    return listaFibonnaci;
+}
+
+console.log(entrada);
