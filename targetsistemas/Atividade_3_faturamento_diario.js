42298b2dd1a6bfad19e70f2d45fc8aa0d95cadae
Elizeu Vailant <el_vailant@hotmail.com>
2022-07-29 14:07:51 -0400

    Começo atividade 3

--- targetsistemas/Atividade_3_faturamento_diario.js
+++ targetsistemas/Atividade_3_faturamento_diario.js
@@ -1,5 +1,20 @@
-faturamentoDiario = [];
+const FATURAMENTO = require('./dados_faturamento.json');
 
-function name(params) {
-    
+
+function calcularDadosFaturamento(historico) {
+    let resultado = {};
+    let valores = [];
+
+    for (let dadosDia of historico) {
+        if (dadosDia.valor == 0) continue;
+        
+        valores.push(dadosDia.valor);
+        console.log(dadosDia);
+    }
+    return {
+        "minimo" : 0
+    };
 }
+
+calcularDadosFaturamento(FATURAMENTO);
+console.log(FATURAMENTO[0].dia);
